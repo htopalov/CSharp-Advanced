b2f1ac1fcb12956e18e26f353be4a002cecab6ef
HTOPALOV <htopalov@gmail.com>
2021-01-14 23:46:03 +0200

    Test upload of two problems from exercise

---
+++ Stacks and Queues Exercise/Stacks and Queues Excercise/Basic Stack Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int elementsPushCount = nsx[0];
+            int elementsPopCount = nsx[1];
+            int elementX = nsx[2];
+            int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < elementsPushCount; i++)
+            {
+                stack.Push(elements[i]);
+            }
+            for (int i = 0; i < elementsPopCount; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(elementX))
+            {
+                Console.WriteLine("true");
+            }
+            else if(stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
