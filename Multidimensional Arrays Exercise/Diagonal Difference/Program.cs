9e09e5c1542e1afcd748f7da6ec1bed643fd6ac6
HTOPALOV <htopalov@gmail.com>
2021-01-17 13:34:27 +0200

    add problem

---
+++ Multidimensional Arrays Exercise/Diagonal Difference/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            int sumLeftDiagonal = 0;
+            int sumRightDiagonal = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        sumLeftDiagonal += matrix[row, col];
+                    }
+                    if ((row + col) == n - 1)
+                    {
+                        sumRightDiagonal += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(sumLeftDiagonal-sumRightDiagonal));;
+        }
+    }
+}
