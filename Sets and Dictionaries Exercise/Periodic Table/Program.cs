d33db55ba3583ec902b1ed8a894b61b1bc479846
HTOPALOV <htopalov@gmail.com>
2021-01-21 19:54:06 +0200

    problems from homework

---
+++ Sets and Dictionaries Exercise/Periodic Table/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicalSet = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] chemicals = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < chemicals.Length; j++)
+                {
+                    chemicalSet.Add(chemicals[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", chemicalSet));
+        }
+    }
+}
