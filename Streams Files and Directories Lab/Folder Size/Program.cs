6e73c49ad80f92c7de0c5a8b9199911d11a04055
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-01-24 13:31:57 +0200

    add problem for folder size

---
+++ Streams Files and Directories Lab/Folder Size/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filesPaths = Directory.GetFiles("TestFolder");
+            double size = 0;
+            foreach (var file in filesPaths)
+            {
+                FileInfo info = new FileInfo(file);
+                size += info.Length;
+            }
+            Console.WriteLine($"Folder size: {size/1024/1024:f2} MB");
+        }
+    }
+}
