43dc35362320c3f587c908cd0366629448110a1a
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-01-31 19:00:11 +0200

    homework

---
+++ Defining Classes Exercise/Defining Classes Exercise/Family.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+//using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+        
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            int maxAge = int.MinValue;
+            Person person = null;
+            foreach (var currentPerson in People)
+            {
+                var currentAge = currentPerson.Age;
+                if (currentAge> maxAge)
+                {
+                    maxAge = currentAge;
+                    person = currentPerson;
+                }
+            }
+            //Person oldestPerson = this.FamilyList.OrderByDescending(x => x.Age).FirstOrDefault();
+            //return oldestPerson;
+            return person;
+        }
+    }
+}
---
+++ Defining Classes Exercise/Defining Classes Exercise/StarUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+            Person oldestMan = family.GetOldestMember();
+            Console.WriteLine($"{oldestMan.Name} {oldestMan.Age}");
+        }
+    }
+}
