36b57ee0ea720b1635841e7127c3e5aafbd9c639
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-05 21:07:57 +0200

    elementsary problems from generics

---
+++ Generics Lab/BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> box;
+        public Box()
+        {
+            box = new List<T>();
+        }
+        public void Add(T element)
+        {
+            box.Add(element);
+        }
+        public T Remove()
+        {
+            var current = box[box.Count-1];
+            box.RemoveAt(box.Count-1);
+            return current;
+        }
+        public int Count
+        {
+            get
+            {
+                return box.Count;
+            }
+        }
+    }
+}
