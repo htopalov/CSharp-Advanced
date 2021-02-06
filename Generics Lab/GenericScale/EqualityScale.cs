060484352b91fbe52337fb370c6a0e0d5b5813fe
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-06 18:47:15 +0200

    lab basic syntax problems

---
+++ Generics Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> 
+        where T : IComparable
+    {
+        private T left;
+        private T right;
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+        public T Left
+        { 
+            get
+            {
+                return left;
+            }
+            set
+            {
+                this.left = value;
+            }
+        }
+
+        public T Right
+        { 
+            get
+            {
+                return right;
+            }
+            set
+            {
+                this.right = value;
+            }
+        }
+        public bool AreEqual()
+        {
+           int result = Left.CompareTo(Right);
+            if (result == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
