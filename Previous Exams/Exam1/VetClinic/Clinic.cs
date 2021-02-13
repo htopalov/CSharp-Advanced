3e50518a8eb3b26f6339c42a6f442e51d097ae84
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-13 23:13:17 +0200

    vet clinic problem

---
+++ Previous Exams/Exam1/VetClinic/Clinic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get { return data.Count; }
+        }
+        public void Add(Pet pet)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Name == name)
+                {
+                    data.Remove(data[i]);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            return data.FirstOrDefault(x => x.Name == name && x.Owner == owner);
+        }
+        public Pet GetOldestPet()
+        {
+            return data.OrderByDescending(x => x.Age).First();
+        }
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var item in data)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Previous Exams/Exam1/VetClinic/Pet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        private string name;
+        private int age;
+        private string owner;
+        public Pet(string name,int age,string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+        public string Name 
+        {
+            get { return name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { this.age = value; }
+        }
+        public string Owner 
+        {
+            get { return owner; }
+            set { this.owner = value; }
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
