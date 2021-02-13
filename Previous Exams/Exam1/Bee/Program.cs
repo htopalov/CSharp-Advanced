b0ea194dbd153bad588258dc433edbbfbfd140a6
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-13 23:10:12 +0200

    exam problems

---
+++ Previous Exams/Exam1/Bee/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] territory = new char[n, n];
+            int beeRow = 0;
+            int beeCol = 0;
+            for (int row = 0; row < territory.GetLength(0); row++)
+            {
+                string inputData = Console.ReadLine();
+                for (int col = 0; col < territory.GetLength(1); col++)
+                {
+                    if (inputData[col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                    territory[row, col] = inputData[col]; 
+                }
+            }
+            string command = Console.ReadLine();
+            int pollinatedFlowersCount = 0;
+            while (command != "End")
+            {
+                switch (command)
+                {
+
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Previous Exams/Exam1/Exam1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lilies = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] roses = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Stack<int> liliesStack = new Stack<int>(lilies);
+            Queue<int> rosesQueue = new Queue<int>(roses);
+            int wreaths = 0;
+            int stored = 0;
+            while(liliesStack.Count>0 && rosesQueue.Count>0)
+            {
+                int lili = liliesStack.Pop();
+                int rose = rosesQueue.Dequeue();
+                if (lili + rose == 15)
+                {
+                    wreaths++;
+                }
+                else if(lili+rose >15)
+                {
+                    while(lili+rose >15)
+                    {
+                        if (lili>=2)
+                        {
+                            lili -= 2;
+                        }
+                        else if (lili <=0)
+                        {
+                            break;
+                        }
+                        if (lili + rose < 15)
+                        {
+                            stored += lili + rose;
+                        }
+                        else if (lili + rose == 15)
+                        {
+                            wreaths++;
+                        }
+                        else if (lili + rose > 15)
+                        {
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    stored += lili + rose;
+                }
+            }
+            wreaths += stored / 15;
+            if (wreaths >=5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5-wreaths} wreaths more!");
+            }
+        }
+    }
+}
