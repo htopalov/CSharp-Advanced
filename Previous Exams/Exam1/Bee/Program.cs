62e7d37815092da32af48d7706f0c4200fb37da3
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-14 11:42:47 +0200

    commit bee

--- Previous Exams/Exam1/Bee/Program.cs
+++ Previous Exams/Exam1/Bee/Program.cs
@@ -20,19 +20,90 @@ namespace Bee
                         beeRow = row;
                         beeCol = col;
                     }
-                    territory[row, col] = inputData[col]; 
+                    territory[row, col] = inputData[col];
                 }
             }
             string command = Console.ReadLine();
-            int pollinatedFlowersCount = 0;
+            int pollinatedCount = 0;
             while (command != "End")
             {
-                switch (command)
+                territory[beeRow, beeCol] = '.';
+                beeRow = BeeMoveRow(command, beeRow);
+                beeCol = BeeMoveCol(command, beeCol);
+                if (!isLost(beeRow, beeCol, n, n))
                 {
-
+                    Console.WriteLine("The bee got lost!");
+                    break;
                 }
+                if (territory[beeRow, beeCol] == 'f')
+                {
+                    pollinatedCount++;
+                }
+                if (territory[beeRow, beeCol] == 'O')
+                {
+                    territory[beeRow, beeCol] = '.';
+                    beeRow = BeeMoveRow(command, beeRow);
+                    beeCol = BeeMoveCol(command, beeCol);
+                    if (territory[beeRow, beeCol] == 'f')
+                    {
+                        pollinatedCount++;
+                    }
+                }
+                territory[beeRow, beeCol] = 'B';
                 command = Console.ReadLine();
             }
+            if (pollinatedCount < 5)
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - pollinatedCount} flowers more");
+            }
+            else
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollinatedCount} flowers!");
+            }
+            for (int row = 0; row < territory.GetLength(0); row++)
+            {
+                for (int col = 0; col < territory.GetLength(1); col++)
+                {
+                    Console.Write(territory[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool isLost(int beeRow, int beeCol, int territoryRows, int territoryCols)
+        {
+            if (beeRow >= territoryRows || beeRow < 0)
+            {
+                return false;
+            }
+            if (beeCol >= territoryCols || beeCol < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static int BeeMoveRow(string direction, int row)
+        {
+            if (direction == "up")
+            {
+                return row - 1;
+            }
+            else if (direction == "down")
+            {
+                return row + 1;
+            }
+            return row;
+        }
+        public static int BeeMoveCol(string direction, int col)
+        {
+            if (direction == "left")
+            {
+                return col - 1;
+            }
+            else if (direction == "right")
+            {
+                return col + 1;
+            }
+            return col;
         }
     }
 }
