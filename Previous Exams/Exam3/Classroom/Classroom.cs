c14570a64b70ec552e9ba0ee5f9c4f434d9a2291
Hristo Topalov <65122521+htopalov@users.noreply.github.com>
2021-02-15 12:56:22 +0200

    commit

---
+++ Previous Exams/Exam3/Classroom/Classroom.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+   public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            this.Capacity = capacity;
+            this.students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+        public int Count { get { return students.Count; }  }
+
+        public string RegisterStudent(Student student)
+        {
+            if (students.Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+        public string DismissStudent(string firstName, string lastName)
+        {
+            var student = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+            if (student != null)
+            {
+                students.Remove(student);
+                return $"Dismissed student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+        public string GetSubjectInfo(string subject)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<Student> listBySubject = students.FindAll(x => x.Subject == subject);
+            if (listBySubject.Count >0)
+            {
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+                foreach (var student in listBySubject)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+                return sb.ToString().Trim();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+        public int GetStudentsCount()
+        {
+            return Count;
+        }
+        public Student GetStudent(string firstName, string lastName)
+        {
+            return students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+        }
+    }
+}
---
+++ Previous Exams/Exam3/Exam3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while (tasks.Count>0)
+            {
+                int task = tasks.Peek();
+                int thread = threads.Peek();
+                if (thread >= task && taskToKill != task)
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                if (taskToKill == task)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToKill}");
+                    Console.WriteLine(string.Join(" ", threads));
+                    break;
+                }
+                else if (thread < task)
+                {
+                    threads.Dequeue();
+                }
+
+            }
+        }
+    }
+}
